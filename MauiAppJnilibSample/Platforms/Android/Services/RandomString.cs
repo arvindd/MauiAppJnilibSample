fb9120d9d66c1accd839a1bcac3447ce357e3326
arvindd <1006084+arvindd@users.noreply.github.com>
2023-03-22 00:04:29 +0530

    Java bindings work.
    However, Visual Studio Intellisense shows error accessing the namespace in the generated DLL.

---
+++ MauiAppJnilibSample/Platforms/Android/Services/RandomString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AndroidRandomString = Org.Mauiappjnilibsample.RandomString;
+
+namespace MauiAppJnilibSample.Platforms.Android.Services
+{
+    internal class RandomString
+    {
+        private AndroidRandomString str = new AndroidRandomString();
+
+        public string Generate()
+        {
+            return str.Generate();
+        }
+    }
+}
--- MauiAppJnilibSample/Services/Java/JavaRandomString.cs
+++ MauiAppJnilibSample/Services/Java/JavaRandomString.cs
@@ -7,19 +7,21 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiAppJnilibSample.Platforms.Android.Services;
 
 namespace MauiAppJnilibSample.Services.Java
 {
     internal class JavaRandomString : RandomStringService
     {
         private readonly Random random = new();
+        private readonly RandomString str = new();
 
         public override IObservable<IChangeSet<string>> Connect()
         {
             return Observable.Generate(0,
                      x => x < 3,
                      x => x + 1,
-                     _ => new string("hello"),
+                     _ => str.Generate(),
                      _ => TimeSpan.FromSeconds(random.Next(1, 2)))
                 .Log(this, "String", x => x)
                 .ToObservableChangeSet();
